221e57f119b5b18618bf8f097b43ba7901fb113a
DiaaHSharqawi <diaasharqawi00@gmail.com>
2025-04-10 12:58:23 +0300

    fix: make traversing search for the company name.

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
+const TARGET_COMPANY = "InfiniteTiers";
+
 var companies = {
   hardware: ["company1", "globalTech", "company2"],
   software: ["Asal", "InfiniteTiers", "Exhausted"],
   networking: ["company3", "company4", "company5"],
 };
 
-// Solution 1:
-console.log(`first solution: using property access  `);
-const bestCompany = companies.software[1];
-console.log(bestCompany);
-console.log(`------`);
-
-// Solution 2:
-//  using the destructuring way
-console.log(`A second solution Using the destructuring`);
-const { software } = companies;
-const [, theBestCompany] = software;
-console.log(theBestCompany);
+function getTargetCompanyName(companiesData, targetCompany) {
+  for (let companiesKey in companiesData) {
+    const companiesName = companiesData[companiesKey];
+    if (companiesName.includes(targetCompany)) {
+      return targetCompany;
+    }
+  }
+  return null;
+}
+const bestCompany = getTargetCompanyName(companies, TARGET_COMPANY);
+console.log(bestCompany ?? "Not found");
